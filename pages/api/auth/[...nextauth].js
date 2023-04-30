d245ac1888163caf3ae8a99396518d73db374c18
Lienkulet <clocicovalexandru@gmail.com>
2023-04-30 16:30:12 +0300

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,30 +1,26 @@
-import NextAuth, { getServerSession } from 'next-auth';
+import NextAuth from 'next-auth';
+import { getServerSession } from 'next-auth/server';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from '@/lib/mongodb';
 import GoogleProvider from 'next-auth/providers/google';
 import { Admin } from '@/models/Admin';
 
 async function isAdmin(email) {
-  const foundAdmin = (await Admin.findOne({email}))
-  if(foundAdmin){
-    return foundAdmin;
-  } else {
-    return false;
-  }
-  // return true;
+  const foundAdmin = await Admin.findOne({ email });
+  return Boolean(foundAdmin);
 }
 
-export const authOptions = {
+const authOptions = {
   secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: async ({session,token,user}) => {
+    session: async ({ session, token, user }) => {
       if (await isAdmin(session?.user?.email)) {
         return session;
       } else {
@@ -36,11 +32,10 @@ export const authOptions = {
 
 export default NextAuth(authOptions);
 
-export async function isAdminRequest(req,res){
-  const session = await getServerSession(req, res, authOptions);
-  if(!(await isAdmin(session?.user?.email))){
-    res.status(401);
-    res.end();
-    throw 'Access not allowed'
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession({ req });
+  if (!(await isAdmin(session?.user?.email))) {
+    res.status(401).end();
+    throw new Error('Access not allowed');
   }
 }
